5303b9676a7235eda8594e582f8d499bebbe9fe7
JenHol1 <jenna@jenna-holland.me.uk>
2024-04-10 23:06:17 +0100

    Fixed error on line 63

--- screen.py
+++ screen.py
@@ -60,7 +60,7 @@ def loop():
             ser.reset_input_buffer()
             ser.write(b'1')
             if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8'),rstrip()
+                line = ser.readline().decode('utf-8').rstrip()
                 print(line)
                 
         else:
