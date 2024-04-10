6d99dca40cd2ee43f20e01d554f70a32f8cc8c76
JenHol1 <153632789+JenHol1@users.noreply.github.com>
2024-04-10 22:51:57 +0100

    Files on RPi and Ard

---
+++ SwiTx_copy_20240216150547.ino
@@ -0,0 +1,79 @@
+/*
+  SwiTx Arduino Code
+  Version 1
+
+
+
+  */
+int inByte ='0';
+const int MuteA = 14;
+const int MuteB = 15;
+const int PlexA1 = 16;
+const int PlexB1 = 18;
+
+void setup() {
+  
+  pinMode(MuteA, OUTPUT);     // 4066 Quad Bilateral Switch
+  digitalWrite(MuteA, HIGH);   // Switch Signal State
+
+  pinMode(MuteB, OUTPUT);     // 4066 Quad Bilateral Switch
+  digitalWrite(MuteB, HIGH);   // Switch Signal State
+
+  pinMode(PlexA1, OUTPUT);     // 4067 Analog Multiplexer
+  digitalWrite(PlexA1, LOW);   // Plexer Signal State 0
+  
+  pinMode(PlexB1, OUTPUT);     // 4067 Analog Multiplexer
+  digitalWrite(PlexB1, LOW);   // Plexer Signal State 0
+
+
+  Serial.begin(9600);      // Serial Port
+  Serial.println("<Serial Test>");     //Serial Port Test Text
+  
+}
+
+void loop() {
+  
+  if (Serial.available() > 0) {
+    inByte = (Serial.read()); // Read byte from serial port
+
+    // Toggle the Device state 
+    if (inByte == '1') {
+      digitalWrite(MuteA, LOW);  // Turn the MuteA ON
+      digitalWrite(MuteB, LOW);  // Turn the MuteB ON
+
+      delay(100);                // Delay 
+
+      digitalWrite(PlexA1, HIGH);  // Turn the PlexA1 ON 1
+      digitalWrite(PlexB1, HIGH);  // Turn the PlexB1 ON 1
+
+      delay(100);                // Delay
+
+      digitalWrite(MuteA, HIGH);  // Turn the MuteA OFF
+      digitalWrite(MuteB, HIGH);  // Turn the MuteB OFF
+      
+      Serial.println("Switched to B");
+
+    } else if (inByte == '0') {
+      digitalWrite(MuteA, LOW);  // Turn the MuteA ON
+      digitalWrite(MuteB, LOW);  // Turn the MuteB ON
+
+      delay(100);                // Delay
+
+      digitalWrite(PlexA1, LOW);  // Turn the PlexA1 ON 0
+      digitalWrite(PlexB1, LOW);  // Turn the PlexB1 ON 0
+
+      delay(100);                // Delay
+
+      digitalWrite(MuteA, HIGH);  // Turn the MuteA OFF
+      digitalWrite(MuteB, HIGH);  // Turn the MuteB OFF
+
+      Serial.println("Switched to A");
+      
+    } else {
+      Serial.println("Invalid character received. Send '1' to turn Switch Output, '0' to turn Keep Current Output.");
+    }
+  }
+}
+
+
+
---
+++ screen.py
@@ -0,0 +1,89 @@
+#Importing libraries
+import serial
+from PCF8574 import PCF8574_GPIO
+from Adafruit_LCD1602 import Adafruit_CharLCD
+from time import sleep, strftime
+from datetime import datetime
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.IN)
+GPIO.setup(22, GPIO.OUT)
+
+def destroy():
+    lcd.clear()
+    
+def get_time_now():     # get system time
+    return datetime.now().strftime('    %H:%M:%S')    
+
+
+def loop():
+    mcp.output(3, 1)     # turn on LCD backlight
+    lcd.begin(16, 2)     # set number of LCD lines and columns
+
+
+    n=4
+    for i in range(0, n):
+        lcd.setCursor(0, 0)    
+        lcd.message("     SwiTx \n") #Startup
+        lcd.message (get_time_now())
+        sleep(1)
+
+    destroy()
+
+
+    while(True):
+            
+        if GPIO.input(27) == 1:
+            print("Input was high")
+            lcd.setCursor(0, 0)  # set cursor position
+            lcd.message ("    Receiving \n")
+            lcd.message("    Output B")
+            sleep(1)
+            GPIO.output(22, GPIO.LOW)
+            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            ser.reset_input_buffer()
+            ser.write(b'0')
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+                print(line)
+
+        if GPIO.input(27) == 0:
+            print("Input was low")
+            lcd.setCursor(0, 0)
+            lcd.message("    Standby    \n")# 
+            lcd.message("    Output A")   
+            sleep(1)
+            GPIO.output(22, GPIO.HIGH)
+            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            ser.reset_input_buffer()
+            ser.write(b'1')
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8'),rstrip()
+                print(line)
+                
+        else:
+            print("Something is fucky wucky")
+
+
+PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
+# Create PCF8574 GPIO adapter.
+try:
+    mcp = PCF8574_GPIO(PCF8574_address)
+except:
+    try:
+        mcp = PCF8574_GPIO(PCF8574A_address)
+    except:
+        print ('I2C Address Error !')
+        exit(1)
+# Create LCD, passing in MCP GPIO adapter.
+lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+
+if __name__ == '__main__':
+    print ('Program is starting ... ')
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
